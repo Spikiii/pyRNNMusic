a96a1c586ac9761c39ffa9f8bd2c5586fac56cea
Eytan Kaplan <eytankaplan101@gmail.com>
2018-02-20 15:05:27 -0600

    "Sucsessful" Navy Seal Copypasta Text Generation
    It got really scottish, and then not that scottish, and now it's in the
    middle.

--- headerRNN.py
+++ headerRNN.py
@@ -8,17 +8,20 @@ from keras.utils import np_utils
 import sys
 #Following a LSTM Text Generation tutorial from <https://machinelearningmastery.com/text-generation-lstm-recurrent-neural-networks-python-keras/>
 
-#settings
+#Settings
 filename = 'Data/navySeal.txt'
-seq_length = 100 #Length of training sequences to feed into the network
+weights_filename = "Checkpoints/weights_09_0.8179.hdf5"
+seq_length = 25 #Length of training sequences to feed into the network
 
-#defs
+#Defs
 raw_text = open(filename).read()
 chars = sorted(list(set(raw_text)))
 char_to_int = dict((c, i) for i, c in enumerate(chars))
 int_to_char = dict((i, c) for i, c in enumerate(chars))
 n_chars = len(raw_text)
 n_vocab = len(chars)
+print("Total Characters: ", n_chars)
+print("Total Vocab: ", n_vocab)
 
 dataX = []
 dataY = []
@@ -30,9 +33,10 @@ for i in range(0, n_chars - seq_length):
     dataX.append([char_to_int[char] for char in seq_in])
     dataY.append(char_to_int[seq_out])
 n_patterns = len(dataX)
+print("Total Patterns: ", n_patterns)
 
-X = numpy.reshape(dataX, (n_patterns, seq_length, 1)) #Reshaping for Keras
-X = X / float(n_vocab) #Normaize the data
+X = numpy.reshape(dataX, (n_patterns, seq_length, 1)) #Reshaping the data for Keras
+X = X / float(n_vocab) #Normalizing the data
 y = np_utils.to_categorical(dataY) #Something about hot encoding?
 
 #Defining the Model
@@ -44,41 +48,39 @@ model.add(Dropout(0.2))
 model.add(Dense(y.shape[1], activation = "softmax")) #Output layer
 model.compile(loss = "categorical_crossentropy", optimizer = "adam")
 
-def train():
+def train(e):
     """Trains the network"""
     #Creating checkpoint system
+    model.load_weights(weights_filename)
     filepath="Checkpoints/weights_{epoch:02d}_{loss:.4f}.hdf5"
     checkpoint = ModelCheckpoint(filepath, monitor = 'loss', verbose = 1, save_best_only = True, mode = 'min')
     callbacks_list = [checkpoint]
 
     #Do the thing!
-    model.fit(X, y, epochs = 20, batch_size = 128, callbacks = callbacks_list)
+    model.fit(X, y, epochs = e, batch_size = 128, callbacks = callbacks_list)
 
 def generate(leng):
     """Generates text"""
     #Load the network weights
-    filename = "Checkpoints/weights_19_3.0567.hdf5"
-    model.load_weights(filename)
+    model.load_weights(weights_filename)
     model.compile(loss = 'categorical_crossentropy', optimizer = 'adam')
 
     # pick a random seed
     start = numpy.random.randint(0, len(dataX) - 1)
     pattern = dataX[start]
-    print("Seed:")
-    print("\"", ''.join([int_to_char[value] for value in pattern]), "\"")
 
+    output = ""
     # generate characters
     for i in range(leng):
         x = numpy.reshape(pattern, (1, len(pattern), 1))
         x = x / float(n_vocab)
         prediction = model.predict(x, verbose=0)
-        print(prediction)
         index = numpy.argmax(prediction)
         result = int_to_char[index]
         seq_in = [int_to_char[value] for value in pattern]
-        sys.stdout.write(result)
+        output += result
         pattern.append(index)
         pattern = pattern[1:len(pattern)]
-    print("Done!")
+    print("".join([int_to_char[value] for value in pattern]) + "|" + output)
 
-generate(100)
+generate(1000)
